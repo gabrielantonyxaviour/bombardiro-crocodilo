7b287a718a9af974e736ec2d645c222e90b7fe68
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2025-04-05 03:54:31 +0800

    world id verification fails

--- frontend/components/verify-world-id.tsx
+++ frontend/components/verify-world-id.tsx
@@ -1,47 +1,88 @@
-'use client'
-import { MiniKit, VerifyCommandInput, VerificationLevel, ISuccessResult } from '@worldcoin/minikit-js'
-import { Button } from './ui/button'
-import { useState } from 'react'
-
+"use client";
+import {
+  MiniKit,
+  VerifyCommandInput,
+  VerificationLevel,
+  ISuccessResult,
+} from "@worldcoin/minikit-js";
+import { Button } from "./ui/button";
+import { use, useEffect, useState } from "react";
+import { WORLD_TESTING_ABI } from "@/lib/constants";
+import { decodeAbiParameters } from "viem";
 
 export async function VerifyWorldId() {
-    const [verified, setVerified] = useState(false)
+  const [verified, setVerified] = useState(false);
+  const [signal, setSignal] = useState<string>("");
 
-    const verifyPayload: VerifyCommandInput = {
-        action: 'test', // This is your action ID from the Developer Portal
-        verification_level: VerificationLevel.Device, // Orb | Device
+  useEffect(() => {
+    console.log("MiniKit is installed", MiniKit.isInstalled());
+    if (MiniKit.isInstalled()) {
+      const signal = MiniKit.user?.walletAddress;
+      if (signal) {
+        setSignal(signal);
+      }
     }
+  }, [MiniKit.user]);
 
-    const handleVerify = async () => {
-        if (!MiniKit.isInstalled()) {
-            console.error('MiniKit is not installed')
-            return
-        }
-        console.log('Verifying World ID')
-        // World App will open a drawer prompting the user to confirm the operation, promise is resolved once user confirms or cancels
-        const { finalPayload } = await MiniKit.commandsAsync.verify(verifyPayload)
-        if (finalPayload.status === 'error') {
-            return console.log('Error payload', finalPayload)
-        }
+  const verifyPayload: VerifyCommandInput = {
+    action: "onboarding",
+    verification_level: VerificationLevel.Orb,
+  };
 
-        // Verify the proof in the backend
-        const verifyResponse = await fetch('/api/verify', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                payload: finalPayload as ISuccessResult, // Parses only the fields we need to verify
-                action: 'test',
-            }),
-        })
-
-        const { success } = await verifyResponse.json()
-        if (success) {
-            console.log('Verification success!')
-            setVerified(true)
-        }
+  const handleVerify = async () => {
+    if (!MiniKit.isInstalled()) {
+      console.error("MiniKit is not installed");
+      return;
     }
+    console.log("Verifying World ID");
+    const { finalPayload } = await MiniKit.commandsAsync.verify({
+      ...verifyPayload,
+      signal,
+    });
+    if (finalPayload.status === "error") {
+      return console.log("Error payload", finalPayload);
+    }
+    console.log("Final payload", finalPayload);
+    // Verify the proof in the backend
+    const verifyResponse = await fetch("/api/verify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        payload: finalPayload as ISuccessResult, // Parses only the fields we need to verify
+        action: "onboarding",
+        signal, // This is the signal we passed in the verify command
+      }),
+    });
 
-    return !verified ? <Button onClick={handleVerify}>Verify World ID</Button> : <p>Verified!</p>
+    const { commandPayload, finalPayload: finalVerifyTxPayload } =
+      await MiniKit.commandsAsync.sendTransaction({
+        transaction: [
+          {
+            address: "0x5E8fbDE101292eeCd9317c32A6b960723100e4D8",
+            abi: WORLD_TESTING_ABI,
+            functionName: "verifyAndExecute",
+            args: [
+              signal,
+              finalPayload.merkle_root,
+              finalPayload.nullifier_hash,
+              decodeAbiParameters(
+                [{ type: "uint256[8]" }],
+                finalPayload.proof as `0x${string}`
+              )[0],
+            ],
+          },
+        ],
+      });
+
+    console.log("Command payload", commandPayload);
+    console.log("Final payload", finalVerifyTxPayload);
+  };
+
+  return !verified ? (
+    <Button onClick={handleVerify}>{MiniKit.user?.walletAddress}</Button>
+  ) : (
+    <p>Verified!</p>
+  );
 }
--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,29 +1,105 @@
 export const chainImageMapping: Record<number, string> = {
-    1: "/chains/eth.svg",
-    11155111: "/chains/eth.svg",
-    43114: "/chains/avax.svg",
-    43113: "/chains/avax.svg",
-    42161: "/chains/arb.png",
-    421614: "/chains/arb.png",
-    8453: "/chains/base.png",
-    84532: "/chains/base.png",
-    480: "/chains/world.png",
-    4801: "/chains/world.png",
-    137: "/chains/pol.png",
-    80002: "/chains/pol.png",
-    10: "/chains/op.png",
-    11155420: "/chains/op.png",
-}
+  1: "/chains/eth.svg",
+  11155111: "/chains/eth.svg",
+  43114: "/chains/avax.svg",
+  43113: "/chains/avax.svg",
+  42161: "/chains/arb.png",
+  421614: "/chains/arb.png",
+  8453: "/chains/base.png",
+  84532: "/chains/base.png",
+  480: "/chains/world.png",
+  4801: "/chains/world.png",
+  137: "/chains/pol.png",
+  80002: "/chains/pol.png",
+  10: "/chains/op.png",
+  11155420: "/chains/op.png",
+};
 
 export const assetImageMapping: Record<string, string> = {
-    'eth': "/chains/eth.svg",
-    'base': "/chains/base.png",
-    'avax': "/chains/avax.svg",
-    'wld': "/chains/world.png",
-    'arb': "/chains/arb.png",
-    'usdc': "/assets/usdc.png",
-    'usdt': "/assets/usdt.png",
-    'dai': "/assets/dai.png",
-    'pol': "/chains/pol.png",
-    'op': "/chains/op.png",
-}
+  eth: "/chains/eth.svg",
+  base: "/chains/base.png",
+  avax: "/chains/avax.svg",
+  wld: "/chains/world.png",
+  arb: "/chains/arb.png",
+  usdc: "/assets/usdc.png",
+  usdt: "/assets/usdt.png",
+  dai: "/assets/dai.png",
+  pol: "/chains/pol.png",
+  op: "/chains/op.png",
+};
+
+export const WORLD_TESTING_ABI = [
+  {
+    inputs: [
+      {
+        internalType: "contract IWorldID",
+        name: "_worldId",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "_appId",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_actionId",
+        type: "string",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "nullifierHash",
+        type: "uint256",
+      },
+    ],
+    name: "DuplicateNullifier",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "nullifierHash",
+        type: "uint256",
+      },
+    ],
+    name: "Verified",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "signal",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "root",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "nullifierHash",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256[8]",
+        name: "proof",
+        type: "uint256[8]",
+      },
+    ],
+    name: "verifyAndExecute",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
